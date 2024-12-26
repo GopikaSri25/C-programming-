1432bdcf8b131e85bbcf8f6daba2c18be16ea793
GopikaSri <gopikasri2k7@gmail.com>
2024-12-26 15:36:23 +0530

    Create Flatten Binary Tree to Linked List.C

---
+++ Flatten Binary Tree to Linked List.C
@@ -0,0 +1,21 @@
+void flatten(struct TreeNode* root){
+    
+    // check if root exists
+    if (root){
+        
+        struct TreeNode* temp = root->right;    // store the right part of root
+        
+        root->right = root->left;           // move the left part to the right
+        root->left = NULL;                  // clear left part
+        
+        struct TreeNode* curr = root;
+        while(curr->right){                 // use while loop to find the bottom right side
+            curr = curr->right;
+        }
+        curr->right = temp;                 // attach temp back
+        
+        // recursion
+        flatten(root->right);
+    }
+
+}
